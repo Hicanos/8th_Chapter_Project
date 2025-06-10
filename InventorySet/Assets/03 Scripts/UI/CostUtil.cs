d6bbdd51edd88883c0733d6f127fa0036a33e051
Hicanos <ruda32168@gmail.com>
2025-06-11 02:09:40 +0900

    ✨ Feat: Big NumCount Util
    큰 수를 계산하는 유틸 추가
    (이제 1000배수마다 a, b, c, 순으로 넘어감)

---
+++ InventorySet/Assets/03 Scripts/UI/CostUtil.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CostUtil
+{
+    private const long ConversionRate = 1000;
+    private static readonly string[] Units = { "", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+
+    public static string ConvertCurrency(double amount)
+    {
+        int unitIndex = 0;
+
+        while (amount >= ConversionRate && unitIndex < Units.Length - 1)
+        {
+            amount /= ConversionRate;
+            unitIndex++;
+        }
+
+        // 첫째 자리까지만 표기
+        string formattedAmount = $"{amount:F1}";
+
+        // 소수점 끝이 0이라면 안보이게
+        formattedAmount = formattedAmount.TrimEnd('0').TrimEnd('.');
+
+        // 값이 처리 후 비어 있다면, 그 값을 '0'으로 설정.
+        if (string.IsNullOrEmpty(formattedAmount))
+        {
+            formattedAmount = "0";
+        }
+
+        return $"{formattedAmount}{Units[unitIndex]}";
+    }
+}
--- InventorySet/Assets/03 Scripts/UI/UIClicker.cs
+++ InventorySet/Assets/03 Scripts/UI/UIClicker.cs
@@ -5,26 +5,30 @@ using TMPro;
 
 public class UIClicker : MonoBehaviour
 {
-    [SerializeField] private int coin;
+    [SerializeField] private long coin;
     [SerializeField] private TMP_Text coinTxt;
+    [SerializeField] private long DebugCoin; // 큰 수 테스트용 코인
 
 
-    public int CurCoin => coin;
+    public long CurCoin => coin;
 
-
+    public void Start()
+    {
+        Reset();
+    }
     public void Reset()
     {
         coin = 0;
-        coinTxt.text = coin.ToString();
+        UpdateUI();
     }
     public void UpdateUI()
     {
-        coinTxt.text = coin.ToString();
+        coinTxt.text = CostUtil.ConvertCurrency(coin);
     }
     public void GetCoin()
     {
         //버튼을 누르면, Coin 증가
-        coin += 1;
+        coin += 1+DebugCoin;
         // coin + 현재 장착중인 아이템 데이터의 CoinBonus 총합
         UpdateUI();
     }
